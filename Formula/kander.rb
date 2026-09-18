5974ae8b2cbdb945440318f75a25ee751763a609
dualface <dualface@gmail.com>
2026-09-18 14:16:10 +0800

    kander 0.7.3

--- Formula/kander.rb
+++ Formula/kander.rb
@@ -1,28 +1,28 @@
 class Kander < Formula
   desc "Kanban orchestration for multiple AI agents"
   homepage "https://github.com/dualface/kander"
-  version "0.7.2"
+  version "0.7.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/dualface/kander/releases/download/v0.7.2/kander-darwin-arm64.tar.gz"
-      sha256 "e9f23bb2d4b0bab1448ebb01a78d37700de9edff90e730c129a3a12440b37422"
+      url "https://github.com/dualface/kander/releases/download/v0.7.3/kander-darwin-arm64.tar.gz"
+      sha256 "ad253ad9a254b4a3e998e46117aa8de71f991ddc307526bf3dfe8071c88c2936"
     end
     on_intel do
-      url "https://github.com/dualface/kander/releases/download/v0.7.2/kander-darwin-amd64.tar.gz"
-      sha256 "e2f1eaedd1a240fdcea13c57b957c72826aa01132be6bdc50010dd536342ecd4"
+      url "https://github.com/dualface/kander/releases/download/v0.7.3/kander-darwin-amd64.tar.gz"
+      sha256 "97353ad7c7fab39ecc79a3f2e4456dfff82a105ec625c2c5acb3ff09a3137f20"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dualface/kander/releases/download/v0.7.2/kander-linux-arm64.tar.gz"
-      sha256 "66144beaef3b0f30c189f200bc14c52c62a09384530f6485ee458cbafb90457a"
+      url "https://github.com/dualface/kander/releases/download/v0.7.3/kander-linux-arm64.tar.gz"
+      sha256 "a0343e89002b7ab27c7bdc70c5ecd1fcc0abade3f04bb66bceb3a85820a2b6d9"
     end
     on_intel do
-      url "https://github.com/dualface/kander/releases/download/v0.7.2/kander-linux-amd64.tar.gz"
-      sha256 "4c7a88e8e2ed59003b42158ff9fd62f66f4d14591893dcf0ab6d3fd74db45e91"
+      url "https://github.com/dualface/kander/releases/download/v0.7.3/kander-linux-amd64.tar.gz"
+      sha256 "760c1468e3d4533bc248060c130b57347875027f6229fa5252047f88fca4d3b7"
     end
   end
 
@@ -31,6 +31,6 @@ class Kander < Formula
   end
 
   test do
-    assert_equal "kander 0.7.2", shell_output("#{bin}/kander version").strip
+    assert_equal "kander 0.7.3", shell_output("#{bin}/kander version").strip
   end
 end

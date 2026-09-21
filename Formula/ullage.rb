b8a67bc31282fa8196b04c6ccab0088573210228
dualface <dualface@gmail.com>
2026-09-21 14:20:18 +0800

    ullage 0.2.0

--- Formula/ullage.rb
+++ Formula/ullage.rb
@@ -1,28 +1,28 @@
 class Ullage < Formula
   desc "Local daemon and CLI for AI subscription usage"
   homepage "https://github.com/dualface/ullage-cli"
-  version "0.1.10"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/dualface/ullage-cli/releases/download/v0.1.10/ullage-aarch64-apple-darwin.tar.gz"
-      sha256 "de34fe5b98dc15332746e250742dd877709d5dc95dc202fd374f6e80620aefed"
+      url "https://github.com/dualface/ullage-cli/releases/download/v0.2.0/ullage-aarch64-apple-darwin.tar.gz"
+      sha256 "896c558c8f1661091e8a1dbd5ad36b959908adbee4cdc9ac3f681fbc20907acc"
     end
     on_intel do
-      url "https://github.com/dualface/ullage-cli/releases/download/v0.1.10/ullage-x86_64-apple-darwin.tar.gz"
-      sha256 "06473c203dfcfb008861dbd17b9b5d6131cd06f2dfd2e9ff5c510d540962e7e4"
+      url "https://github.com/dualface/ullage-cli/releases/download/v0.2.0/ullage-x86_64-apple-darwin.tar.gz"
+      sha256 "8f215d7c82f410c6fd3f2030ef02c2c71f05d9c58ecfa94b4ed006dd4ad298a4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dualface/ullage-cli/releases/download/v0.1.10/ullage-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a3291dff33a67a7d3561d88e230efd87c029294ddd0d9f01f934efeeb427cbf1"
+      url "https://github.com/dualface/ullage-cli/releases/download/v0.2.0/ullage-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "bd579c9e11d7e535f7cae0d70d493bb457222b16ea36fe004f843939a8e7ae5e"
     end
     on_intel do
-      url "https://github.com/dualface/ullage-cli/releases/download/v0.1.10/ullage-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2b3eb754d188909f3fc1d1167486572d763a62e988f0ef08d47ad1b5e56c3a41"
+      url "https://github.com/dualface/ullage-cli/releases/download/v0.2.0/ullage-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "9fa5267200e6c55a2e9474259f095e5f49684b1752c60a8d72d7d52660d4f036"
     end
   end
 
